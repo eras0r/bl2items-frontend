685a6dec74728a158a9a2a2a0d6c6363916049ce
eras0r <er4s0r@gmail.com>
2014-05-25 21:53:55 +0200

    migrated shield and weapons view to ui-router

--- app/scripts/shield/shield-def.js
+++ app/scripts/shield/shield-def.js
@@ -1,25 +1,32 @@
 /**
  * module definition for the 'shield' module.
  */
-define(['angular', 'angular-route', 'angular-resource'], function (angular) {
+define(['angular', 'angular-ui-router', 'angular-resource'], function (angular) {
 
     /**
-     * Module definition for the rarity module.
+     * Module definition for the shield module.
      */
     var shieldModule = angular.module('shieldModule', [
-        'ngRoute',
+        'ui.router',
         'ngResource'
     ])
-        .config(function ($routeProvider) {
-            $routeProvider.
-                when('/shields', {
+        .config(function ($stateProvider) {
+            $stateProvider
+                .state('shields', {
+                    abstract: true,
+                    url: "/shields",
+                    template: '<ui-view />'
+                })
+                .state('shields.list', {
+                    url: "/list",
                     templateUrl: '/scripts/item/item-list/item-list.html',
                     controller: 'ShieldListCtrl'
                 })
-                .when('/shields/create', {
+                .state('shields.create', {
+                    url: "/create",
                     templateUrl: '/scripts/shield/shield-details.html',
                     controller: 'ShieldCreateCtrl'
-                })
+                });
         })
         .constant('SHIELD_RESOURCE_URL', 'http://localhost/bl2items-backend/shields/:id');
 
--- app/scripts/weapon/weapon-def.js
+++ app/scripts/weapon/weapon-def.js
@@ -1,25 +1,34 @@
 /**
  * module definition for the 'weapon' module.
  */
-define(['angular', 'angular-route', 'angular-resource'], function (angular) {
+define(['angular', 'angular-ui-router', 'angular-resource'], function (angular) {
+
+    'use strict';
 
     /**
      * Module definition for the rarity module.
      */
     var weaponModule = angular.module('weaponModule', [
-        'ngRoute',
+        'ui.router',
         'ngResource'
     ])
-        .config(function ($routeProvider) {
-            $routeProvider.
-                when('/weapons', {
+        .config(function ($stateProvider) {
+            $stateProvider
+                .state('weapons', {
+                    abstract: true,
+                    url: "/weapons",
+                    template: '<ui-view />'
+                })
+                .state('weapons.list', {
+                    url: "/list",
                     templateUrl: '/scripts/item/item-list/item-list.html',
                     controller: 'WeaponListCtrl'
                 })
-                .when('/weapons/create', {
+                .state('weapons.create', {
+                    url: "/create",
                     templateUrl: '/scripts/weapon/weapon-details.html',
                     controller: 'WeaponCreateCtrl'
-                })
+                });
         })
         .constant('WEAPON_RESOURCE_URL', 'http://localhost/bl2items-backend/weapons/:id');
 
