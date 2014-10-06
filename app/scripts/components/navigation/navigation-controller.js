3b7f5777e1ba4582458df7c2c0a25e435664e148
eras0r <er4s0r@gmail.com>
2014-10-06 19:32:31 +0200

    replaced navigation methods with ui-router's ui-sref directive

--- app/scripts/components/navigation/navigation-controller.js
+++ app/scripts/components/navigation/navigation-controller.js
@@ -3,38 +3,7 @@ define(['angular', 'components/navigation/navigation-def'], function (angular, n
     'use strict';
 
     navigationModule
-        .controller('NavCtrl', ['$scope', '$location', function ($scope, $location) {
-            $scope.showItems = function () {
-                $location.path('/items');
-            };
-
-            $scope.showWeapons = function () {
-                $location.path('/weapons');
-            };
-
-            $scope.showShields = function () {
-                $location.path('/shields');
-            };
-
-            $scope.showBrands = function () {
-                $location.path('brands');
-            };
-
-            $scope.createWeapon = function () {
-                $location.path('/weapons/create');
-            };
-
-            $scope.showRarities = function () {
-                $location.path('/rarities');
-            };
-
-            $scope.showManufacturers = function () {
-                $location.path('/manufacturers');
-            };
-
-            $scope.showDamageTypes = function () {
-                $location.path('/damageTypes');
-            };
+        .controller('NavCtrl', ['$scope', '$state', function ($scope, $state) {
 
         }]);
 
