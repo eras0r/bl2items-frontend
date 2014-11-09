aea9e258a864d52faba3fba6b189b74dd1e41073
eras0r <er4s0r@gmail.com>
2014-11-09 01:53:43 +0100

    jshint fixes

--- app/scripts/components/highlight-text/highlight-text-filter.js
+++ app/scripts/components/highlight-text/highlight-text-filter.js
@@ -5,20 +5,20 @@ define(['angular', 'components/highlight-text/highlight-text-def'], function (an
     highlightTextModule.filter('highlightText', function () {
         return function (input) {
             var mapObj = {
-                "{c}": "<span class=\"elem-corrosive\">",
-                "{/c}": "</span>",
-                "{e}": "<span class=\"elem-electro\">",
-                "{/e}": "</span>",
-                "{x}": "<span class=\"elem-explosive\">",
-                "{/x}": "</span>",
-                "{f}": "<span class=\"elem-fire\">",
-                "{/f}": "</span>",
-                "{s}": "<span class=\"elem-slag\">",
-                "{/s}": "</span>",
-                "\n": "<br />"
+                '{c}': '<span class="elem-corrosive">',
+                '{/c}': '</span>',
+                '{e}': '<span class="elem-electro">',
+                '{/e}': '</span>',
+                '{x}': '<span class="elem-explosive">',
+                '{/x}': '</span>',
+                '{f}': '<span class="elem-fire">',
+                '{/f}': '</span>',
+                '{s}': '<span class="elem-slag">',
+                '{/s}': '</span>',
+                '\n': '<br />'
             };
-            var re = new RegExp(Object.keys(mapObj).join("|"), "gi");
-            if (input == null) {
+            var re = new RegExp(Object.keys(mapObj).join('|'), 'gi');
+            if (input === null) {
                 return null;
             }
             return input.replace(re, function (matched) {
