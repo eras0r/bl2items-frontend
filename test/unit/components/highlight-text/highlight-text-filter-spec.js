58a0b278efe9a13c5ab544a5e81fc369ba0bbc47
eras0r <er4s0r@gmail.com>
2015-03-21 00:31:47 +0100

    add unit test for highlightText filter

---
+++ test/unit/components/highlight-text/highlight-text-filter-spec.js
@@ -0,0 +1,47 @@
+define(['angular', 'angular-mocks', 'components/highlight-text/highlight-text-filter'], function (angular, mocks) {
+
+    'use strict';
+
+    describe('Unit: Testing Filters', function () {
+
+        var filterName = 'highlightText';
+
+        beforeEach(module('highlightTextModule'));
+
+        it('should have a highlight-textfilter', inject(function ($filter) {
+            expect($filter(filterName)).not.toEqual(null);
+        }));
+
+        it('should return the original text if it does not contain special tags',
+            inject(function ($filter) {
+                var inputText = 'some text without tags';
+                var filteredText = $filter(filterName)(inputText);
+                expect(filteredText).toEqual(inputText);
+            })
+        );
+
+        function testElementalHighlight(tagName, elementalDamage) {
+            it('should highlight "' + elementalDamage + '" text',
+                inject(function ($filter) {
+                    // opening and cosing tags
+                    var filteredText = $filter(filterName)('{' + tagName + '}' + elementalDamage + '{/' + tagName + '}');
+                    expect(filteredText).toEqual('<span class="elem-' + elementalDamage + '">' + elementalDamage + '</span>');
+
+                    // opening tags only
+                    filteredText = $filter(filterName)('{' + tagName + '}' + elementalDamage);
+                    expect(filteredText).toEqual('<span class="elem-' + elementalDamage + '">' + elementalDamage);
+                })
+            );
+        }
+
+        describe('it should highlight elemental damage types', function () {
+            testElementalHighlight('c', 'corrosive');
+            testElementalHighlight('e', 'electro');
+            testElementalHighlight('x', 'explosive');
+            testElementalHighlight('f', 'fire');
+            testElementalHighlight('s', 'slag');
+        });
+
+    });
+
+});
