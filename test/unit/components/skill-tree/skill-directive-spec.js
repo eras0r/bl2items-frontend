adb29d0d1902b20f4e2332c396785c0fcacc84f7
eras0r <er4s0r@gmail.com>
2015-03-29 13:39:08 +0200

    refactor unit tests for directives

--- test/unit/components/skill-tree/skill-directive-spec.js
+++ test/unit/components/skill-tree/skill-directive-spec.js
@@ -3,60 +3,73 @@ define([
     'angular-mocks',
     'components/skill-tree/skill-tree-module-inc',
     'components/skill-tree/skill.html'
-], function (angular, mocks) {
+], function (angular, mocks, testModule) {
 
     'use strict';
 
-    describe('Directive: Skill', function () {
-        var $compile, $rootScope;
+    var moduleName = 'skillTreeModule';
+    var directiveName = 'skill';
 
-        beforeEach(module('skillTreeModule'));
-        beforeEach(module('scripts/components/skill-tree/skill.html'));
+    var $compile;
+    var $rootScope;
 
-        beforeEach(inject(
-            ['$compile', '$rootScope', function ($c, $r) {
-                $compile = $c;
-                $rootScope = $r;
+    describe('Unit testing', function () {
+        describe('Module: ' + moduleName, function () {
+            describe('Directive: ' + directiveName, function () {
+                // load the module
+                beforeEach(module(moduleName));
 
-                $rootScope.skill = {
-                    'name': 'Locked and Loaded',
-                    'levels': 5,
-                    'image': 'locked-and-loaded.png',
-                    'text': 'Reloading your gun gives you +5% Fire Rate per level for a short time.',
-                    'points': 0
-                };
-                $rootScope.characterClass = 'Gunzerker';
-                $rootScope.subTree = 'green';
-            }]
-        ));
+                // load templates
+                // TODO provide templates as angular module called 'templates'
+                beforeEach(module('scripts/components/skill-tree/skill.html'));
 
-        it('should display the correct skill image as background image', function () {
-            var element = $compile('<bl2-skill data-skill="skill" data-sub-tree="{{subTree}}" data-characater-class="characterClass" data-on-add="addPoint(skill)" data-on-remove="removePoint(skill)"></bl2-skill>')($rootScope);
-            $rootScope.$digest();
+                beforeEach(inject(
+                    ['$compile', '$rootScope', function ($c, $r) {
+                        $compile = $c;
+                        $rootScope = $r;
 
-            var backgroundImage = element.find('.skill-image').css('background-image');
-            expect(backgroundImage).toMatch('images/dynamic/skills/gunzerker/green/locked-and-loaded.png');
-        });
+                        $rootScope.skill = {
+                            'name': 'Locked and Loaded',
+                            'levels': 5,
+                            'image': 'locked-and-loaded.png',
+                            'text': 'Reloading your gun gives you +5% Fire Rate per level for a short time.',
+                            'points': 0
+                        };
+                        $rootScope.characterClass = 'Gunzerker';
+                        $rootScope.subTree = 'green';
+                    }]
+                ));
 
-        it('should display the correct points image for level 1 skills', function () {
-            $rootScope.skill.levels = 1;
-            $rootScope.skill.points = 1;
+                it('should display the correct skill image as background image', function () {
+                    var element = $compile('<bl2-skill data-skill="skill" data-sub-tree="{{subTree}}" data-characater-class="characterClass" data-on-add="addPoint(skill)" data-on-remove="removePoint(skill)"></bl2-skill>')($rootScope);
+                    $rootScope.$digest();
 
-            var element = $compile('<bl2-skill data-skill="skill" data-sub-tree="{{subTree}}" data-characater-class="characterClass" data-on-add="addPoint(skill)" data-on-remove="removePoint(skill)"></bl2-skill>')($rootScope);
-            $rootScope.$digest();
+                    var backgroundImage = element.find('.skill-image').css('background-image');
+                    expect(backgroundImage).toMatch('images/dynamic/skills/gunzerker/green/locked-and-loaded.png');
+                });
 
-            var img = element.find('img');
-            expect(img).not.toBe(null);
-            expect(img.attr('src')).toBe('images/dynamic/skills/points/1-1.png');
-        });
+                it('should display the correct points image for level 1 skills', function () {
+                    $rootScope.skill.levels = 1;
+                    $rootScope.skill.points = 1;
 
-        it('should display the correct points image for level 5 skills', function () {
-            var element = $compile('<bl2-skill data-skill="skill" data-sub-tree="{{subTree}}" data-characater-class="characterClass" data-on-add="addPoint(skill)" data-on-remove="removePoint(skill)"></bl2-skill>')($rootScope);
-            $rootScope.$digest();
+                    var element = $compile('<bl2-skill data-skill="skill" data-sub-tree="{{subTree}}" data-characater-class="characterClass" data-on-add="addPoint(skill)" data-on-remove="removePoint(skill)"></bl2-skill>')($rootScope);
+                    $rootScope.$digest();
 
-            var img = element.find('img');
-            expect(img).not.toBe(null);
-            expect(img.attr('src')).toBe('images/dynamic/skills/points/0-5.png');
+                    var img = element.find('img');
+                    expect(img).not.toBe(null);
+                    expect(img.attr('src')).toBe('images/dynamic/skills/points/1-1.png');
+                });
+
+                it('should display the correct points image for level 5 skills', function () {
+                    var element = $compile('<bl2-skill data-skill="skill" data-sub-tree="{{subTree}}" data-characater-class="characterClass" data-on-add="addPoint(skill)" data-on-remove="removePoint(skill)"></bl2-skill>')($rootScope);
+                    $rootScope.$digest();
+
+                    var img = element.find('img');
+                    expect(img).not.toBe(null);
+                    expect(img.attr('src')).toBe('images/dynamic/skills/points/0-5.png');
+                });
+
+            });
         });
     });
 
