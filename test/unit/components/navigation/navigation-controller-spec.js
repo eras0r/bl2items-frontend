6b673f2ff2baa4903f0b87bcdcd8a07157397da0
eras0r <er4s0r@gmail.com>
2015-06-30 12:11:57 +0200

    add unit tests for navigation controller

--- test/unit/components/navigation/navigation-controller-spec.js
+++ test/unit/components/navigation/navigation-controller-spec.js
@@ -8,8 +8,11 @@ define([
 
     var moduleName = 'rn.navigation';
     var controllerName = 'NavigationCtrl';
-    var controller;
+
+    var vm;
     var $scope;
+
+    var NavigationService, SessionService;
 
     describe('Unit testing', function () {
         describe('Module: ' + moduleName, function () {
@@ -20,19 +23,49 @@ define([
                     module(moduleName);
 
                     // initialize the controller and a mock scope
-                    beforeEach(inject(function ($controller, $rootScope) {
+                    inject(function ($controller, $rootScope, _NavigationService_, _SessionService_) {
+
+                        NavigationService = _NavigationService_;
+                        SessionService = _SessionService_;
+
+                        spyOn(SessionService, 'getCurrentUser').and.returnValue();
+                        spyOn(SessionService, 'isUserLoggedIn').and.returnValue();
+
                         $scope = $rootScope.$new();
-                        controller = $controller(controllerName, {
+                        vm = $controller(controllerName, {
                             $scope: $scope
                         });
-                    }));
+                    });
                 });
 
                 it('should be registered', function () {
-                    expect(controller).not.toBeUndefined();
+                    expect(vm).not.toBeUndefined();
                 });
 
-                // TODO add unit tests
+                it('should contain a navItems array with one single entry upon creation', function () {
+                    expect(vm.navItems).toBeArray();
+                    expect(vm.navItems.length).toBe(1);
+                    expect(vm.navItems).toContain({
+                        sortOrder: 50,
+                        label: 'navigation.admin.title',
+                        group: 'bl2.admin',
+                        role: 'admin',
+                        items: []
+                    });
+                });
+
+                it('should provide a getCurrentUser method', function () {
+                    vm.getCurrentUser();
+
+                    expect(SessionService.getCurrentUser).toHaveBeenCalled();
+                });
+
+                it('should provide an isUserLoggedIn method', function () {
+                    vm.isUserLoggedIn();
+
+                    expect(SessionService.isUserLoggedIn).toHaveBeenCalled();
+                });
+
             });
         });
     });
