8aed97527d7d3d9880a631e86c5f69488d28beca
Steven Heller <er4s0r@gmail.com>
2016-03-05 01:33:00 +0100

    implement global error handling mechanism and message service to display messages
    - introduce MessageService
    - create a view within index.html to display messages
    - create global error handler for restangular
    - create global error hanndler for ui-router state change errors

---
+++ app/scripts/components/error-handling/error-handling-module-inc.js
@@ -0,0 +1,14 @@
+/**
+ * bootstraps the 'errorHandling' module.
+ */
+define([
+    'components/error-handling/error-handling-module-def',
+    'components/error-handling/message-service',
+    'components/error-handling/message-controller'
+], function (errorHandlingModule) {
+
+    'use strict';
+
+    return errorHandlingModule;
+
+});
