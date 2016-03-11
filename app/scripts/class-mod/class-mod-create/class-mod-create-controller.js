5e144eb15db4610e4c515cf2c3d0a1701c7e7f32
Steven Heller <er4s0r@gmail.com>
2016-03-11 20:58:44 +0100

    remove $scope from class mod module

--- app/scripts/class-mod/class-mod-create/class-mod-create-controller.js
+++ app/scripts/class-mod/class-mod-create/class-mod-create-controller.js
@@ -6,102 +6,111 @@ define([
     'use strict';
 
     classModModule.controller('ClassModCreateCtrl', [
-        '$scope', '$log', 'rarities', 'manufacturers', 'characters', 'ClassModService', 'CharacterService',
-        function ($scope, $log, rarities, manufacturers, characters, ClassModService, CharacterService) {
+        '$log', 'rarities', 'manufacturers', 'characters', 'ClassModService', 'CharacterService', ClassModCreateCtrl]);
 
-            $scope.loadSkillTree = function () {
-                CharacterService.getSkills($scope.classMod.characterClass).then(function (skills) {
-                    $scope.skills = skills;
+    /** @ngInject */
+    function ClassModCreateCtrl($log, rarities, manufacturers, characters, ClassModService, CharacterService) {
 
-                    // reset selected skills
-                    $scope.classMod.skills = {};
+        var vm = this;
 
-                    // init point property for each skill within the skill tree
+        vm.loadSkillTree = loadSkillTree;
+        vm.addPoint = addPoint;
+        vm.removePoint = removePoint;
+        vm.save = save;
 
-                    // iterate sub trees
-                    angular.forEach($scope.skills, function (subTree) {
-                        // iterate skillTiers
-                        angular.forEach(subTree.skillTiers, function (tier) {
-                            if (tier.leftSkill) {
-                                tier.leftSkill.points = 0;
-                            }
-                            if (tier.middleSkill) {
-                                tier.middleSkill.points = 0;
-                            }
-                            if (tier.rightSkill) {
-                                tier.rightSkill.points = 0;
-                            }
-                        });
+        function loadSkillTree() {
+            CharacterService.getSkills(vm.classMod.characterClass).then(function (skills) {
+                vm.skills = skills;
+
+                // reset selected skills
+                vm.classMod.skills = {};
+
+                // init point property for each skill within the skill tree
+
+                // iterate sub trees
+                angular.forEach(vm.skills, function (subTree) {
+                    // iterate skillTiers
+                    angular.forEach(subTree.skillTiers, function (tier) {
+                        if (tier.leftSkill) {
+                            tier.leftSkill.points = 0;
+                        }
+                        if (tier.middleSkill) {
+                            tier.middleSkill.points = 0;
+                        }
+                        if (tier.rightSkill) {
+                            tier.rightSkill.points = 0;
+                        }
                     });
                 });
-            };
+            });
+        }
 
-            $scope.addPoint = function (skill) {
-                // first skill point added?
-                if (!$scope.classMod.skills[skill.name]) {
-                    // add new selected skill entry
-                    $scope.classMod.skills[skill.name] = skill;
-                }
+        function addPoint(skill) {
+            // first skill point added?
+            if (!vm.classMod.skills[skill.name]) {
+                // add new selected skill entry
+                vm.classMod.skills[skill.name] = skill;
+            }
 
-                // spend one point if the skills level has not been reached
-                if (skill.points < skill.levels) {
-                    skill.points++;
-                }
-            };
+            // spend one point if the skills level has not been reached
+            if (skill.points < skill.levels) {
+                skill.points++;
+            }
+        }
 
-            $scope.removePoint = function (skill) {
-                // remove one spent point
-                if (skill.points > 0) {
-                    skill.points--;
-                }
+        function removePoint(skill) {
+            // remove one spent point
+            if (skill.points > 0) {
+                skill.points--;
+            }
 
-                // last skill point removed?
-                if (skill.points === 0) {
-                    delete $scope.classMod.skills[skill.name];
-                }
-            };
+            // last skill point removed?
+            if (skill.points === 0) {
+                delete vm.classMod.skills[skill.name];
+            }
+        }
 
-            $scope.save = function () {
-                ClassModService.create($scope.classMod)
-                    .then(function () {
-                        ClassModService.showList();
-                    }, function (response) {
-                        if (response.status === 422) {
-                            $scope.errors = response.data;
-                        }
-                        else {
-                            // TODO show error message
-                            $log.error('error creating class mod');
-                        }
-                    });
-            };
+        function save() {
+            ClassModService.create(vm.classMod)
+                .then(function () {
+                    ClassModService.showList();
+                }, function (response) {
+                    if (response.status === 422) {
+                        vm.errors = response.data;
+                    }
+                    else {
+                        // TODO show error message
+                        $log.error('error creating class mod');
+                    }
+                });
+        }
 
-            // constructor
-            // assign ui router resolves
-            $scope.rarities = rarities;
-            $scope.manufacturers = manufacturers;
-            $scope.characters = characters;
+        // constructor
+        // assign ui router resolves
+        vm.rarities = rarities;
+        vm.manufacturers = manufacturers;
+        vm.characters = characters;
 
-            // init default classMod
-            $scope.classMod = {
-                itemtype: 'classMod',
-                level: 50,
-                rarity: $scope.rarities[0],
-                manufacturer: $scope.manufacturers[0],
-                characterClass: $scope.characters[0],
-                damage: null,
-                accuracy: null,
-                fireRate: null,
-                reloadSpeed: null,
-                magazineSize: null,
-                uniqueText: null,
-                elementalText: null,
-                additionalText: null,
-                skills: {}
-            };
+        // init default classMod
+        vm.classMod = {
+            itemtype: 'classMod',
+            level: 50,
+            rarity: vm.rarities[0],
+            manufacturer: vm.manufacturers[0],
+            characterClass: vm.characters[0],
+            damage: null,
+            accuracy: null,
+            fireRate: null,
+            reloadSpeed: null,
+            magazineSize: null,
+            uniqueText: null,
+            elementalText: null,
+            additionalText: null,
+            skills: {}
+        };
 
-            $scope.loadSkillTree();
+        vm.loadSkillTree();
 
-        }]);
+    }
 
 });
